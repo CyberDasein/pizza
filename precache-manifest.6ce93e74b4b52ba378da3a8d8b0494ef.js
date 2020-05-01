02ef50651a0fba45e8a4eff22b2df038b543b211
Alex <CyberDasein>
2020-05-01 15:57:31 +0500

    Updates

---
+++ precache-manifest.6ce93e74b4b52ba378da3a8d8b0494ef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4cfbdd00fd0fdd669c77f92e576f716",
+    "url": "/pizza/index.html"
+  },
+  {
+    "revision": "8559c0c53ba38a7764f8",
+    "url": "/pizza/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "655ac0c105333140452a",
+    "url": "/pizza/static/css/main.4b505550.chunk.css"
+  },
+  {
+    "revision": "8559c0c53ba38a7764f8",
+    "url": "/pizza/static/js/2.9c401d01.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pizza/static/js/2.9c401d01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "655ac0c105333140452a",
+    "url": "/pizza/static/js/main.bb9a5d19.chunk.js"
+  },
+  {
+    "revision": "d62fa667c74fd46d57c6",
+    "url": "/pizza/static/js/runtime-main.48791bff.js"
+  },
+  {
+    "revision": "ab2a8d9523ef48dad4e68d927182387b",
+    "url": "/pizza/static/media/226.ab2a8d95.png"
+  },
+  {
+    "revision": "03f06345bd2db9018a2a6032d04c0b6c",
+    "url": "/pizza/static/media/231.03f06345.png"
+  },
+  {
+    "revision": "35affef4c1d43ee0abed24ff7acd113b",
+    "url": "/pizza/static/media/233.35affef4.png"
+  }
+]);
